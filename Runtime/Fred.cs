40059b71caa689fc1a50e7a6991ac921a57c1224
Anthony Rosenbaum <anthony.rosenbaum@unity3d.com>
2021-06-15 09:40:59 -0500

    moved to folder

---
+++ Runtime/Fred.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Fred : MonoBehaviour
+{
+
+    public Button but = null;
+    public string value = "Fred";
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(!but)
+        {
+            but = GetComponent<Button>();
+        }
+        if (but)
+        {
+            Text t = GetComponentInChildren<Text>();
+            t.text = value;
+        }
+    }
+
+}
